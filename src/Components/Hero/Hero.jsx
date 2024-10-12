8e29d6a719a9988022f87a590446e07e2cc31da0
Angela Keri <nabaweesi3579@gmail.com>
2024-10-12 21:34:33 +0100

    wording1

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,9 +6,9 @@ const Hero = () => {
   return (
     <div className='hero container'>
         <div className="hero-text">
-            <h1>Flavish Hampers - Elevating Every Occasion</h1><br/>
+            <h1>Flavish - Elevating Every Occasion</h1><br/>
             <a href="#contact"><button className='btn'>Place Order <img src={dark_arrow}/></button></a><br/>
-            <p>Welcome to Flavish Hampers, where elegance meets thoughtfulness in every gift, floral arrangement, and decor piece. Life may be demanding, 
+            <p>Welcome to Flavish, where elegance meets thoughtfulness in every gift, floral arrangement, and decor piece. Life may be demanding, 
               but we believe in making your loved ones feel special and valued with creations that inspire joy and beauty.</p>
         </div>
     </div>

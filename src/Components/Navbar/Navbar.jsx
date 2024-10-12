cb9777daf4305dbfdaa38888b2c3f6038cbdcd9c
Angela Keri <nabaweesi3579@gmail.com>
2024-10-12 14:57:58 +0100

    logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
-import KamuliLogo from '../../assets/KamuliLogo.png'
+import FlavishLogo from '../../assets/FlavishLogo.png'
 import menu_icon from '../../assets/menu-icon.png'
 import { Link } from 'react-scroll'
 
@@ -24,7 +24,7 @@ const Navbar = () => {
 
   return (
     <nav className={`container ${sticky? 'dark-nav' : ''}`}>
-        <img src={KamuliLogo} alt="" className="logo"/>
+        <img src={FlavishLogo} alt="" className="logo"/>
         <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
           <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
           <li><Link to='program' smooth={true} offset={-260} duration={500}>Programs</Link></li>
